57059d67fdd0f0bc44307e0c44ef7aa635df0f40
lluvidali <ntm7557@gmail.com>
2021-09-07 18:06:08 +0900

    ログイン機能の実装、バリデーションの設定
    ◾️バリデーション
    ・パスワード：半角英数字混合
    ・名前：漢字、ひらがな、カタカナ
    ・名前（カナ）：カタカナ入力

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,          presence: true
-  validates :first_name_kanji,  presence: true
-  validates :family_name_kanji, presence: true
-  validates :first_name_kana,   presence: true
-  validates :family_name_kana,  presence: true
+  validates :first_name_kanji,  presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name_kanji, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name_kana,   presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :family_name_kana,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birthday,          presence: true
-
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
