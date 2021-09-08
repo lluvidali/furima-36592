0559118aa73a7572d4a1f3a253fc93a7ee0ce4e9
lluvidali <ntm7557@gmail.com>
2021-09-08 17:25:05 +0900

    Rubocop実施

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,15 +3,15 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-    email {Faker::Internet.free_email}
+    email { Faker::Internet.free_email }
     password = Faker::Internet.password(mix_case: false)
-    password {password}
-    password_confirmation {password}
-    nickname {Faker::Name.name}
+    password { password }
+    password_confirmation { password }
+    nickname { Faker::Name.name }
     first_name_kanji { person.first.kanji }
     family_name_kanji { person.last.kanji }
     first_name_kana { person.first.katakana }
     family_name_kana { person.last.katakana }
-    birthday { Faker::Date.backward}
+    birthday { Faker::Date.backward }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
- 
+
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
       it 'nickname,email,password,first_name_kanji,family_name_kanji,first_name_kana,family_name_kana,birthdayが存在すれば登録できる' do
@@ -17,7 +17,6 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
     end
-  
 
     context '新規登録できない場合' do
       it 'nickenameが空では登録できない' do
@@ -45,7 +44,7 @@ RSpec.describe User, type: :model do
       it 'first_name_kanji（名前）が全角（漢字・ひらがな・カタカナ）でなければ登録できない' do
         @user.first_name_kanji = 'a'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kanji is invalid")
+        expect(@user.errors.full_messages).to include('First name kanji is invalid')
       end
 
       it 'family_name_kanji（名字）が空では登録できない' do
@@ -57,7 +56,7 @@ RSpec.describe User, type: :model do
       it 'family_name_kanji（名字）が全角（漢字・ひらがな・カタカナ）でなければ登録できない' do
         @user.family_name_kanji = 'a'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family name kanji is invalid")
+        expect(@user.errors.full_messages).to include('Family name kanji is invalid')
       end
 
       it 'first_name_kana（名前）が空では登録できない' do
@@ -68,8 +67,8 @@ RSpec.describe User, type: :model do
 
       it 'first_name_kana（名前）が全角（カタカナ）でなければ登録できない' do
         @user.first_name_kana = 'a'
-        @user.valid? 
-        expect(@user.errors.full_messages).to include("First name kana is invalid")
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name kana is invalid')
       end
 
       it 'family_name_kana（名字）が空では登録できない' do
@@ -80,8 +79,8 @@ RSpec.describe User, type: :model do
 
       it 'family_name_kana（名字）が全角（カタカナ）でなければ登録できない' do
         @user.family_name_kana = 'a'
-        @user.valid? 
-        expect(@user.errors.full_messages).to include("Family name kana is invalid")
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Family name kana is invalid')
       end
 
       it 'birthday（生年月日）が空では登録できない' do
@@ -101,20 +100,20 @@ RSpec.describe User, type: :model do
         @user.save
         another_user = FactoryBot.build(:user, email: @user.email)
         another_user.valid?
-        expect(another_user.errors.full_messages).to include("Email has already been taken")
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
 
       it 'emailに@を含まない場合は登録できない' do
         @user.email = 'abcde.yahoo'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
 
       it 'passwordに半角英数字の両方が含まれていない場合登録できない' do
         @user.password = '123456'
         @user.password_confirmation = '123456'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include('Password is invalid')
       end
 
       it 'passwordが５文字以下では登録できない' do
@@ -126,4 +125,3 @@ RSpec.describe User, type: :model do
     end
   end
 end
-
