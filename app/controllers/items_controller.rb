cd379b4c070819c8d40b8f1c5798972534d5cc75
lluvidali <ntm7557@gmail.com>
2021-09-12 01:56:31 +0900

    Active Storageの導入
    ・画像加工のツールの導入
    ・has_one_attachedメソッドの記述
    ・itemsテーブルに画像ファイルを紐付け

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def image_params
+    params.require(:items).permit(:product, :category_id, :price, :delivery_fee_id, :content, :status_id, :prefecture_id, :shipping_days_id, :image).merge(user_id: current_user_id)
 end
