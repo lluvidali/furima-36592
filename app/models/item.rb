2b528c9da30b10441680544b70573ee9cde2c29a
lluvidali <ntm7557@gmail.com>
2021-09-12 03:05:34 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,14 @@
 class Item < ApplicationRecord
+  validates :product,          presence: true
+  validates :category_id,      presence: true
+  validates :price,            presence: true
+  validates :delivery_fee_id,  presence: true
+  validates :content,          presence: true
+  validates :status_id,        presence: true
+  validates :prefecture_id,    presence: true
+  validates :shipping_days_id, presence: true
 
+  validates :category_id, :delivery_fee_id, :status_id, :prefecture_id, :shipping_days_id, numericality: { other_than: 1 , message: "can't be blank" }
 
   belongs_to :user
   has_one_attached :image
