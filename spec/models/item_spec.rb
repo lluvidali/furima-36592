6fbc89a63d0424932e4ca6bb31606268da86479d
lluvidali <ntm7557@gmail.com>
2021-09-14 11:48:17 +0900

    修正①
    ・rubucop実施

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -68,15 +68,51 @@ RSpec.describe Item, type: :model do
       end
 
       it '¥300未満の数字だと保存できない' do
-        @item.price = '100'
+        @item.price = 1 * 100
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
 
       it '¥10,000,000以上の数字だと保存できない' do
-        @item.price = '10000000'
+        @item.price = 1 * 100_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.category_id = 'id: 1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it '商品の状態に「---」が選択されている場合は出品できない' do
+        @item.status_id = 'id: 1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+
+      it '配送料の負担に「---」が選択されている場合は出品できない' do
+        @item.delivery_fee_id = ' id: 1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
+      end
+
+      it '発送元の地域に「---」が選択されている場合は出品できない' do
+        @item.prefecture_id = 'id: 1 '
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
+        @item.shipping_day_id = 'id: 1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+
+      it '価格に半角数字以外が含まれている場合は出品できない' do
+        @item.price = 'あああ'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
     end
   end
