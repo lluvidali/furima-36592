11d87c7754aa81a828489ab91ec9d49a051b019a
lluvidali <ntm7557@gmail.com>
2021-09-15 13:51:57 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
   resources :items
+
+  root to: 'orders#index'
+  resources :orders, only:[:create]
+
+  root to: 'buy_infos#index'
+  resources :buy_infos, only:[:create]
 end
