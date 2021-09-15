13977751c3a0581d159f9c5235102e2dcf6fa8e9
lluvidali <ntm7557@gmail.com>
2021-09-15 13:50:32 +0900

    バリデーションの設定

--- app/models/buy_info.rb
+++ app/models/buy_info.rb
@@ -1,2 +1,15 @@
 class BuyInfo < ApplicationRecord
+
+  validates :postal_code, presence: true
+  validates :address,     presence: true
+  validates :tel,         presence: true
+
+  validates :prefecture_id { other_than: 1, message: "can't be blank" }
+
+  belongs_to :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 end
+
+
