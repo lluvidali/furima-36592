0822213944c4994ace31a6b1d451170b0a0a5754
lluvidali <ntm7557@gmail.com>
2021-09-21 18:11:03 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,6 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @order_info = OrderInfo.new
-    redirect_to action: :index unless @item.user_id == current_user.id
     if @item.order != nil 
     redirect_to root_path 
     end
